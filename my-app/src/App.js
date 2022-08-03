2455b0940ca692159052bdd60dbd189fa2814a6e
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-03 19:50:28 +0530

    committed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,16 @@ import React from 'react'
 
  const App = () => {
   return (
-    <div><h1>
-      HWKLLO</h1>
+    <div>
+      <img src="react-logo.png" width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
       </div>
   )
 }
