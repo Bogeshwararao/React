4f4c419a820d11364189c9bd3c9aee3f9174675f
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-08-04 10:30:38 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from "./components/Nav"
+// import Main from "./components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Nav />
+            
+        </div>
+    )
+}
